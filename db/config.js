9fb2870bb83f54f32aa77d86eef85c64aac51253
amanqs <116822442+amanqs@users.noreply.github.com>
2024-09-19 22:43:13 +0700

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,13 +1,13 @@
-export const token = '6443639684:AAGxJBhL8XmmhFwoM87H4TAIf8PP3OSlgo8' // ambil dari bot father
-export const owner = ['1592956557'] //Ganti , jika ingin lebih dari 1 nomer tambahkan menjadi ['1234xx', '123xx', '5687xx'] 
-export const username = 'rimokoD8Vjbg'//Ganti ambil Usernamekey dari digiflazz
-export const apiKey = 'dev-9ed5f980-5658-11ef-af00-8935e16e792e' //Ganti ambil Productionkey dari digiflazz
-export const namarekening = 'Feriansyah Rusmana' // Isi dengan nama rekening mu
+export const token = '8155158423:AAH0FtUt2Sw1d-1ntiOoiBDK5LOWAZ1Hx4o' // ambil dari bot father
+export const owner = ['753569421'] //Ganti , jika ingin lebih dari 1 nomer tambahkan menjadi ['1234xx', '123xx', '5687xx'] 
+export const username = 'yawemegNA0pW'//Ganti ambil Usernamekey dari digiflazz
+export const apiKey = 'dev-b85a9de0-4448-11ef-a6ce-2d9b32379bc0' //Ganti ambil Productionkey dari digiflazz
+export const namarekening = 'Que Sierra Mentari Ammarkhan' // Isi dengan nama rekening mu
 
 // Paydisini Apikey
-export const paydisini_apikey = "b0389a2c06b630398e50eb45b0493819" //Ganti ambil dari APIKEY Paydisini
-export const paydisini_nomer = '085143050110' //Ganti sesuai nomor yang terdaftar pada Paydisini
-export const minimalDepoOtomatis = '5000' //Ganti jika ingin minimal depositnya ditambah
+export const paydisini_apikey = "3ee691271def4d2009cfdd13d45a74fb" //Ganti ambil dari APIKEY Paydisini
+export const paydisini_nomer = '088221010546' //Ganti sesuai nomor yang terdaftar pada Paydisini
+export const minimalDepoOtomatis = '100' //Ganti jika ingin minimal depositnya ditambah
 export const maximalDepoOtomatis = '1000000' //Ganti jika ingin maksimal depositnya ditambah
 
 //Medanpedia Apikey
