a60084a246d28e6fd229b55f84d1d2a382ec83ac
Lapor 86 <laporpolri86@gmail.com>
2024-12-10 05:04:09 +0700

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,4 +1,4 @@
-export const token = '8155158423:AAH0FtUt2Sw1d-1ntiOoiBDK5LOWAZ1Hx4o' // ambil dari bot father
+export const token = '6710723152:AAHJuo1gQc0pzLWDK3gCq9j6sTx3ATxIm_s' // ambil dari bot father
 export const owner = ['753569421'] //Ganti , jika ingin lebih dari 1 nomer tambahkan menjadi ['1234xx', '123xx', '5687xx'] 
 export const username = 'yawemegNA0pW'//Ganti ambil Usernamekey dari digiflazz
 export const apiKey = 'dev-b85a9de0-4448-11ef-a6ce-2d9b32379bc0' //Ganti ambil Productionkey dari digiflazz
